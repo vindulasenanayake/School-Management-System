3ff72be808b2ac2331c75b2d7503d9d9ce452015
charinipeiris <30485000+charinipeiris@users.noreply.github.com>
2018-12-25 18:50:49 +0530

    Update chek.php
    profile changed

--- chek.php
+++ chek.php
@@ -1,9 +1,5 @@
 <?php
-	$username="";
-	$names="";
-	$age="";
-	$address="";
-	$class ="";
+	
 
 	if(isset($_POST['search']))
 	{
@@ -11,10 +7,13 @@
 
 		$connect =mysqli_connect("localhost","root","","school_mgt_system");
 
-		$query = "SELECT 'names','address','class','age',FROM 'student_info' WHERE 'username'=$username";
+		$query = "SELECT 'names','address','class','age'FROM 'student_info' WHERE 'username'=$username";
 
 		$result = mysqli_query($connect, $query);
 
+		if(mysqli_num_rows($result)>0){
+
+		
 		while ($row = mysqli_fetch_array($result))
 		 {
            $username= $row ['username'];
@@ -23,6 +22,17 @@
 	       $address=['address'];
 	       $class =['class'];
 		}
+
+		}else{
+			echo "undefined ID";
+		$username="";
+	    $names="";
+	    $age="";
+	    $address="";
+	    $class ="";
+
+
+	}
 
 		mysqli_free_result($result);
 	    mysqli_close($connect);
@@ -43,14 +53,66 @@
 
 <html>
 <head>
+	<head>
+        <link href="css/style1.css" rel="stylesheet" type="text/css">
+        <title> Student Profile | Lumbini College</title>
+        <link rel="icon" href="images/logo1.png" type="image/png">
+        <style>
+            body {
+            background-image: url("images/sh.jpg");
+            background-attachment: fixed;
+            background-position:0;
+            background-repeat: no-repeat;
+            background-size: cover;
+          }
+        </style>
 </head>
 <body>
-	<form action="chek.php" methods="post">
-		Username:<input type="text" name="username" ><br><br>
-		Name:<input type="text" name="names"  value="<?php echo $names;?>"> <br><br>
-		Class:<input type="text" name="class"  value="<?php echo $class;?>"><br><br>
-		Age:<input type="text" name="age"  value="<?php echo $age;?>"><br><br>
-		Address:<input type="text" name="address"  value="<?php echo $address;?>"><br><br>
+	<form action
+	
+	="chek.php" methods="post">
+         
+        <div class="col-6">                  
+            <div class="card-profile">
+                <div class="row">
+                    <div class="col-12 card-heading-blue">
+                        <center><br><h1>Student Profile</h1> <br></center>
+                    </div>
+                </div>
+                       
+        <hr>
+		 <div class="row">                                
+            <div class="col-3 lbl">Username</div>
+		<div class="col-9">
+			<input type="text" name="username" >
+		</div>
+	    </div>
+		<div class="row">                                
+            <div class="col-3 lbl">Name</div>
+		<div class="col-9">
+			<input type="text" name="names"  value="<?php echo $names;?>"> <br><br>
+		</div>
+	    </div>
+		<div class="row">                                
+            <div class="col-3 lbl">Class</div>
+		<div class="col-9">
+			<input type="text" name="class"  value="<?php echo $class;?>"><br><br>
+		</div>
+	    </div>
+
+		<div class="row">                                
+            <div class="col-3 lbl">Age</div>
+
+		<div class="col-9">
+			<input type="text" name="age"  value="<?php echo $age;?>"><br><br>
+		</div>
+	    </div>
+		<div class="row">                                
+            <div class="col-3 lbl">Address</div>
+		<div class="col-9">
+			<input type="text" name="address"  value="<?php echo $address;?>"><br><br>
+		</div>
+	    </div>
 		<input type ="submit" name="search" value="Find"><br><br>
 	</form>
 </body>
