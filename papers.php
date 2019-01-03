7f0a453097d8d8eec0e332aea90a1765c9b18e52
vindula senanayake <vindulasenanayake@gmail.com>
2019-01-03 21:50:32 +0530

    edited upload view part

---
+++ papers.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<form method="POST" enctype="multipart/form-data" action="upload.php">
+    <input type="file" name="file">
+    <input type="submit" value="Upload">
+	
+</form>
+
+<?php
+
+// Creating file to upload
+// Displaying all uploaded files
+
+$files = scandir("uploads");
+for ($a = 2; $a < count($files); $a++){
+	// Displaying links to download
+	// Making it downloadable 
+	?>
+	<p>
+	   <a download="<?php echo $files[$a]?>" href="uploads/<?php echo $files[$a]?>"><?php echo $files[$a]?></a>
+	</p>
+	<?php
+}
